cf493d0d119be23737ab8cab22fcaaf6536ad77e
Azeez Sulaimon <111203778+sulaimonazeez@users.noreply.github.com>
2026-07-02 01:23:04 +0100

    Enhance Transaction model with JSON serialization
    Add JSON transformation for Transaction model

--- models/Transaction.js
+++ models/Transaction.js
@@ -20,4 +20,20 @@ transactionSchema.index({ user: 1, createdAt: -1 });
 transactionSchema.index({ user: 1, type: 1 });
 transactionSchema.index({ createdAt: -1 });
 
+transactionSchema.set('toJSON', {
+  virtuals: true,
+  transform: (doc, ret) => {
+    ret.id = ret._id;
+    ret.balance_before = ret.balanceBefore;
+    ret.balance_after = ret.balanceAfter;
+    ret.reference_doc = ret.referenceDoc;
+    ret.reference_type = ret.referenceType;
+    ret.created_at = ret.createdAt;
+    ret.updated_at = ret.updatedAt;
+    delete ret._id;
+    delete ret.__v;
+    return ret;
+  },
+});
+
 module.exports = mongoose.model('Transaction', transactionSchema);
