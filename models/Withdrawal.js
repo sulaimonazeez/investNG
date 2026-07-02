4d478006ebc3b4119bf45ca229adfbff56e877ce
Azeez Sulaimon <111203778+sulaimonazeez@users.noreply.github.com>
2026-07-02 01:13:47 +0100

    Enhance Withdrawal schema with JSON transformation
    Added JSON transformation for Withdrawal model.

--- models/Withdrawal.js
+++ models/Withdrawal.js
@@ -16,4 +16,22 @@ const withdrawalSchema = new mongoose.Schema({
 withdrawalSchema.index({ user: 1, status: 1 });
 withdrawalSchema.index({ status: 1, createdAt: -1 });
 
+withdrawalSchema.set('toJSON', {
+  virtuals: true,
+  transform: (doc, ret) => {
+    ret.id = ret._id;
+    ret.bank_name = ret.bankName;
+    ret.account_number = ret.accountNumber;
+    ret.account_name = ret.accountName;
+    ret.admin_note = ret.adminNote;
+    ret.approved_by = ret.approvedBy;
+    ret.approved_at = ret.approvedAt;
+    ret.created_at = ret.createdAt;
+    ret.updated_at = ret.updatedAt;
+    delete ret._id;
+    delete ret.__v;
+    return ret;
+  },
+});
+
 module.exports = mongoose.model('Withdrawal', withdrawalSchema);
